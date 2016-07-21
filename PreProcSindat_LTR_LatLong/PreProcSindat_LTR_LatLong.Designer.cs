a36996150a6b529672ecb44aeb03183c5f3ce282
RomuloMenezes <romulo.menezes@gmail.com>
2016-07-21 16:20:48 -0300

    Stable version

---
+++ PreProcSindat_LTR_LatLong/PreProcSindat_LTR_LatLong.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace PreProcSindat_LTR_LatLong
+{
+    partial class PreProcSindat_LTR_LatLong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(271, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 40);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(271, 144);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.InitialDirectory = "D:\\_GIT\\Projetos\\SINDAT";
+            // 
+            // PreProcSindat_LTR_LatLong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 199);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "PreProcSindat_LTR_LatLong";
+            this.Text = "PreProcSindat_LTR_LatLong";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ PreProcSindat_LTR_LatLong/PreProcSindat_LTR_LatLong.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PreProcSindat_LTR_LatLong
+{
+    public partial class PreProcSindat_LTR_LatLong : Form
+    {
+        public PreProcSindat_LTR_LatLong()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Please select a source file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                FileStream inputStream = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+                StreamReader inputFile = new StreamReader(inputStream, System.Text.Encoding.UTF8);
+                FileStream outputStream = new FileStream(openFileDialog1.InitialDirectory + "\\TidyData.csv", FileMode.Create, FileAccess.Write);
+                StreamWriter outputFile = new StreamWriter(outputStream,System.Text.Encoding.UTF8);
+                string currInputLine = "";
+                string currOutputLine = "";
+                string[] columns = new string[14];
+                string[] latLong;
+                string[] splitLatLong;
+                string currLat;
+                string currLong;
+                int verticesIndex;
+                int seqIndex;
+
+                outputFile.WriteLine("Linha;Tensão;Seq;Lat;Long"); // Writes header
+                inputFile.ReadLine(); // Skip header
+
+                while ((currInputLine = inputFile.ReadLine()) != null)
+                {
+                    columns=currInputLine.Split(';');
+                    if (columns[1] != "")
+                    {
+                        columns[13] = columns[13].Replace(", ", ",").Trim();
+                        latLong = columns[13].Split(',');
+                        seqIndex = 0;
+                        for (verticesIndex = 0; verticesIndex < latLong.Length; verticesIndex++)
+                        {
+                            splitLatLong = latLong[verticesIndex].Split(' ');
+                            currLat = splitLatLong[0];
+                            currLong = splitLatLong[1];
+                            currOutputLine = columns[1] + ";" + columns[3] + " " + columns[4] + ";" + seqIndex + ";" + currLat.Replace(".", ",") + ";" + currLong.Replace(".", ",");
+                            outputFile.WriteLine(currOutputLine);
+                            seqIndex++;
+                        }
+                    }
+                }
+                inputFile.Close();
+                outputFile.Close();
+                Cursor.Current = Cursors.Default;
+                MessageBox.Show("Data tidied up", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
